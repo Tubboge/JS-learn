5725f332e0376825d7ed510f3082f43af8ac0e4e
Tubboge_ (Pepe Wightmore) <pepe.wightmore@protonmail.com>
2022-11-16 00:21:52 +0000

    added code to program.js

--- program.js
+++ program.js
@@ -1,1 +1,10 @@
-console.log('gittest')
+const test = true;
+let idk = false;
+while (!idk) {
+    console.log('idk = false')
+    idk = test
+}
+while (test) {
+    console.log('test = true')
+    idk = false
+}
